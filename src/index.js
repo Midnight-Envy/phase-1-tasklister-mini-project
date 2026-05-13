d1af7ecff732c4f1b0e2f3ec6698019ca0092c0e
Thomas Correia <thomas.correiap@gmail.com>
2026-05-13 15:29:28 -0400

    Implemented functional todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector(`form`).addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector(`input`);
+    const value = input.value;
+    input.value = "";
+    const li = document.createElement(`li`);
+    li.textContent = value;
+    document.querySelector(`ul`).appendChild(li);
+})
+})
