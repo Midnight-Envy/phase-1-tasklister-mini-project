92bf7b63827855cdce8587120f026b1cf25200d5
Thomas Correia <thomas.correiap@gmail.com>
2026-05-13 15:36:30 -0400

    Edited structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(`form`).addEventListener("submit", (event) => {
     event.preventDefault();
-    const input = document.querySelector(`input`);
-    const value = input.value;
-    input.value = "";
-    const li = document.createElement(`li`);
-    li.textContent = value;
+    let input = document.querySelector(`input`);
+    let value = input.value;
+        input.value = "";
+    let li = document.createElement(`li`);
+        li.textContent = value;
     document.querySelector(`ul`).appendChild(li);
-})
+  })
 })
